d9d4d4fc964c11b1a8e62e489d6068158056d2a5
Jebu Ittiachen <jebu@jebu.net>
2023-03-12 13:14:02 +0530

    rewire package

--- packages/wallet/utils/testSig.ts
+++
@@ -1,4 +0,0 @@
-import { WebAuthnUtils } from './WebAuthnUtils';
-
-let res = WebAuthnUtils.getMessageSignature("MEQCIGDjO93JkbcbYSgIUrdF79T_B9zsTF-PPU_VwXWQDYfXAiBvnL7zAxzs4eJAYOAxlwk7mrSvAi4E6Pk14EdSVtwiog==");
-console.log(res);
